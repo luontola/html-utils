2e2075dd02bba732b215abc5e2989ad9812e1af1
Esko Luontola <esko@luontola.fi>
2025-02-25 16:58:08 +0200

    Code reformat

--- test/html-templates.test.ts
+++ test/html-templates.test.ts
@@ -1,31 +1,31 @@
-import {describe, expect, test} from 'vitest'
+import {describe, expect, test} from "vitest"
 import {html} from "../src/html-templates.js"
 
-describe('html templates', () => {
+describe("html templates", () => {
 
-    test('static html', () => {
+    test("static html", () => {
         expect(html`<p>foo</p>`).toStrictEqual({html: "<p>foo</p>"})
     })
 
-    test('html with placeholders', () => {
+    test("html with placeholders", () => {
         const a = "foo"
         const b = "bar"
         const c = "gazonk"
         expect(html`${a}<span>${b}</span>${c}`).toStrictEqual({html: "foo<span>bar</span>gazonk"})
     })
 
-    test('escapes HTML special characters in string placeholders', () => {
+    test("escapes HTML special characters in string placeholders", () => {
         const a = "<>&'\""
         expect(html`${a}`).toStrictEqual({html: "&lt;&gt;&amp;&#39;&quot;"})
     })
 
-    test('inserts HTML placeholders as-is', () => {
+    test("inserts HTML placeholders as-is", () => {
         const a = html`<span>foo</span>`
         expect(html`<p>${a}</p>`).toStrictEqual({html: "<p><span>foo</span></p>"})
     })
 
-    test('encodes object placeholders to JSON', () => {
+    test("encodes object placeholders to JSON", () => {
         const a = {foo: 123}
-        expect(html`<p>${a}</p>`).toStrictEqual({html: '<p>{"foo":123}</p>'})
+        expect(html`<p>${a}</p>`).toStrictEqual({html: "<p>{\"foo\":123}</p>"})
     })
 })
--- test/html-testing.test.ts
+++ test/html-testing.test.ts
@@ -1,39 +1,39 @@
-import {describe, expect, test} from 'vitest'
-import {visualizeHtml} from "../src/html-testing.js";
+import {describe, expect, test} from "vitest"
+import {visualizeHtml} from "../src/html-testing.js"
 
-describe('visualizeHtml', () => {
+describe("visualizeHtml", () => {
 
-    test('empty input', () => {
-        expect(visualizeHtml('')).toBe('');
-        expect(visualizeHtml(null)).toBe('');
-        expect(visualizeHtml(undefined)).toBe('');
-    });
+    test("empty input", () => {
+        expect(visualizeHtml("")).toBe("")
+        expect(visualizeHtml(null)).toBe("")
+        expect(visualizeHtml(undefined)).toBe("")
+    })
 
-    test('normalizes whitespace', () => {
-        expect(visualizeHtml(' a\n\t\rb    c ')).toBe('a b c');
-    });
+    test("normalizes whitespace", () => {
+        expect(visualizeHtml(" a\n\t\rb    c ")).toBe("a b c")
+    })
 
-    test('replaces HTML tags with whitespace', () => {
-        expect(visualizeHtml('<p>one</p><p>two</p>')).toBe('one two');
-    });
+    test("replaces HTML tags with whitespace", () => {
+        expect(visualizeHtml("<p>one</p><p>two</p>")).toBe("one two")
+    })
 
-    test('inline elements will not add spacing to text', () => {
-        expect(visualizeHtml('x<a>y</a>z')).toBe('xyz');
-        expect(visualizeHtml('x<a><abbr><b><big><cite><code><em><i><small><span><strong><tt>y</tt></strong></span></small></i></em></code></cite></big></b></abbr></a>z')).toBe('xyz');
-    });
+    test("inline elements will not add spacing to text", () => {
+        expect(visualizeHtml("x<a>y</a>z")).toBe("xyz")
+        expect(visualizeHtml("x<a><abbr><b><big><cite><code><em><i><small><span><strong><tt>y</tt></strong></span></small></i></em></code></cite></big></b></abbr></a>z")).toBe("xyz")
+    })
 
-    test('replaces HTML character entities', () => {
-        expect(visualizeHtml('1&nbsp;000')).toBe('1 000');
-        expect(visualizeHtml('&lt;')).toBe('<');
-        expect(visualizeHtml('&gt;')).toBe('>');
-        expect(visualizeHtml('&amp;')).toBe('&');
-        expect(visualizeHtml('&quot;')).toBe('"');
-        expect(visualizeHtml('&apos;')).toBe("'");
-        expect(visualizeHtml('&#39;')).toBe("'");
-    });
+    test("replaces HTML character entities", () => {
+        expect(visualizeHtml("1&nbsp;000")).toBe("1 000")
+        expect(visualizeHtml("&lt;")).toBe("<")
+        expect(visualizeHtml("&gt;")).toBe(">")
+        expect(visualizeHtml("&amp;")).toBe("&")
+        expect(visualizeHtml("&quot;")).toBe("\"")
+        expect(visualizeHtml("&apos;")).toBe("'")
+        expect(visualizeHtml("&#39;")).toBe("'")
+    })
 
-    test('elements with the data-test-icon attribute are replaced with its value', () => {
-        expect(visualizeHtml('<input type="checkbox" data-test-icon="☑️" checked value="true">')).toBe('☑️');
-        expect(visualizeHtml('x<div data-test-icon="🟢">y</div>z')).toBe('x 🟢 y z');
-    });
-});
+    test("elements with the data-test-icon attribute are replaced with its value", () => {
+        expect(visualizeHtml(`<input type="checkbox" data-test-icon="☑️" checked value="true">`)).toBe("☑️")
+        expect(visualizeHtml(`x<div data-test-icon="🟢">y</div>z`)).toBe("x 🟢 y z")
+    })
+})
