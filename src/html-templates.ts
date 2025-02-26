27e91d4216512bd7849b764092381ab9254d4dc2
Esko Luontola <esko@luontola.fi>
2025-02-26 12:57:32 +0200

    Refactor

--- src/html-templates.ts
+++ src/html-templates.ts
@@ -33,19 +33,14 @@ function placeholderToHtmlString(placeholder: any): string {
     if (placeholder === null) {
         return ""
     }
-    if (Array.isArray(placeholder)) {
-        let html = ""
-        for (const item of placeholder) {
-            html += placeholderToHtmlString(item)
-        }
-        return html
-    }
     if (typeof placeholder === "object") {
         if (typeof placeholder.html === "string") {
             return placeholder.html
-        } else {
-            return escapeHtml(JSON.stringify(placeholder))
         }
+        if (Array.isArray(placeholder)) {
+            return placeholder.map(placeholderToHtmlString).join("")
+        }
+        return escapeHtml(JSON.stringify(placeholder))
     }
     return escapeHtml(placeholder)
 }
