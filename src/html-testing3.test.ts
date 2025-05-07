2e3df362db59c70edb9d541faa49c29d4b0ddc5d
Esko Luontola <esko@luontola.fi>
2025-05-07 19:35:15 +0300

    Copy html-testing2

---
+++ src/html-testing3.test.ts
@@ -0,0 +1,96 @@
+import {describe, expect, test} from "vitest"
+import {visualizeHtml} from "./html-testing3.js"
+import {html} from "./html-templates.js"
+import React from "react"
+import {renderToStaticMarkup} from "react-dom/server"
+
+// Vendored from https://github.com/luontola/html-utils
+
+describe("visualizeHtml", () => {
+
+    test("empty input", () => {
+        expect(visualizeHtml("")).toBe("")
+        expect(visualizeHtml(null)).toBe("")
+        expect(visualizeHtml(undefined)).toBe("")
+    })
+
+    test("normalizes whitespace", () => {
+        expect(visualizeHtml(" a\n\t\rb    c ")).toBe("a b c")
+    })
+
+    test("replaces HTML tags with whitespace", () => {
+        expect(visualizeHtml("<p>one</p><p>two</p>")).toBe("one two")
+    })
+
+    test("inline elements will not add spacing to text", () => {
+        expect(visualizeHtml("x<a>y</a>z")).toBe("xyz")
+        expect(visualizeHtml("x<a><abbr><b><big><cite><code><em><i><small><span><strong><tt>y</tt></strong></span></small></i></em></code></cite></big></b></abbr></a>z")).toBe("xyz")
+        expect(visualizeHtml(`x<a\nhref=""\n>y</a>z`), "works with newlines between attributes").toBe("xyz")
+    })
+
+    test("hides style elements", () => {
+        expect(visualizeHtml(`
+            <style>
+                p { color: red; }
+            </style>`)).toBe("")
+        expect(visualizeHtml(`
+            <style type="text/css">
+                p { color: red; }
+            </style>`), "with type attribute").toBe("")
+    })
+
+    test("hides comments", () => {
+        expect(visualizeHtml("<!-- foo -->")).toBe("")
+        expect(visualizeHtml("foo<!-- 666 -->bar")).toBe("foobar")
+        expect(visualizeHtml("<!-- > -->"), "matches until the end of comment, instead of the first > character").toBe("")
+        expect(visualizeHtml("<!--\n>\n-->"), "works with newlines in the comment").toBe("")
+    })
+
+    test("replaces HTML character entities", () => {
+        expect(visualizeHtml("1&nbsp;000")).toBe("1 000")
+        expect(visualizeHtml("&lt;")).toBe("<")
+        expect(visualizeHtml("&gt;")).toBe(">")
+        expect(visualizeHtml("&amp;")).toBe("&")
+        expect(visualizeHtml("&quot;")).toBe("\"")
+        expect(visualizeHtml("&apos;")).toBe("'")
+        expect(visualizeHtml("&#39;")).toBe("'")
+    })
+
+    test("data-test-icon attribute is shown before the element", () => {
+        expect(visualizeHtml(`<input type="checkbox" data-test-icon="☑️" checked value="true">`)).toBe("☑️")
+        expect(visualizeHtml(`x<div data-test-icon="🟢">y</div>z`), "spacing, block elements").toBe("x 🟢 y z")
+        expect(visualizeHtml(`x<span data-test-icon="🟢">y</span>z`), "spacing, inline elements").toBe("x 🟢 yz")
+        expect(visualizeHtml(`<div\ndata-test-icon="🟢"\n></div>`), "works with newlines between attributes").toBe("🟢")
+    })
+
+    test("data-test-content attribute replaces the element's content", () => {
+        expect(visualizeHtml(`<textarea data-test-content="[foo]">foo</textarea>`)).toBe("[foo]")
+        expect(visualizeHtml(`x<div data-test-content="🟢">y</div>z`), "spacing, block elements").toBe("x 🟢 z")
+        expect(visualizeHtml(`x<span data-test-content="🟢">y</span>z`), "spacing, inline elements").toBe("x🟢z")
+    })
+
+    test("data-test-icon and data-test-content can coexist", () => {
+        expect(visualizeHtml(`x<div data-test-icon="A" data-test-content="B">y</div>z`), "spacing, block elements").toBe("x A B z")
+        expect(visualizeHtml(`x<span data-test-icon="A" data-test-content="B">y</span>z`), "spacing, inline elements").toBe("x A Bz")
+    })
+
+    test("works for DOM elements", () => {
+        const a = document.createElement("p")
+        a.textContent = "foo"
+        expect(visualizeHtml(a)).toBe("foo")
+
+        a.setAttribute("data-test-icon", "🟢")
+        expect(visualizeHtml(a), "uses the element's outerHTML").toBe("🟢 foo")
+    })
+
+    test("works for our HTML templates without unwrapping", () => {
+        const a = html`<p>foo</p>`
+        expect(visualizeHtml(a)).toBe("foo")
+    })
+
+    test("works for React elements", () => {
+        const a = React.createElement("p", null, "foo")
+        expect(renderToStaticMarkup(a)).toBe("<p>foo</p>")
+        expect(visualizeHtml(a)).toBe("foo")
+    })
+})
---
+++ src/html-testing3.ts
@@ -0,0 +1,73 @@
+import type {Html} from "./html-templates.js"
+import type React from "react"
+import {isValidElement} from "react"
+import {renderToStaticMarkup} from "react-dom/server"
+
+// Vendored from https://github.com/luontola/html-utils
+
+export function visualizeHtml(html: string | null | undefined | Element | Html | React.ReactElement): string {
+    if (!html) {
+        return ""
+    }
+    if (typeof html !== "string") {
+        // support DOM elements
+        if (html instanceof Element) {
+            return visualizeHtml(html.outerHTML)
+        }
+        // support our HTML templates
+        if ("html" in html) {
+            return visualizeHtml(html.html)
+        }
+        // support React
+        if (isValidElement(html)) {
+            return visualizeHtml(renderToStaticMarkup(html))
+        }
+        throw new TypeError() // should be unreachable
+    }
+    const document = new DOMParser().parseFromString(html, "text/html")
+
+    // custom visualization using data-test-icon attribute
+    document.querySelectorAll("[data-test-icon]").forEach(el => {
+        el.before(` ${el.getAttribute("data-test-icon")} `)
+    })
+
+    // custom visualization using data-test-content attribute
+    document.querySelectorAll("[data-test-content]").forEach(el => {
+        el.textContent = el.getAttribute("data-test-content")
+    })
+
+    // hidden elements
+    document.querySelectorAll("style").forEach(el => {
+        el.remove()
+    })
+    iterateComments(document, node => node.remove())
+
+    html = document.documentElement.outerHTML
+
+    // strip all HTML tags
+    html = html.replace(/<\/?(a|abbr|b|big|cite|code|em|i|small|span|strong|tt)\b.*?>/sg, "") // inline elements
+        .replace(/<[^>]*>/g, " ")  // block elements
+
+    // replace HTML character entities
+    html = html.replace(/&nbsp;/g, " ")
+        .replace(/&lt;/g, "<") // must be after stripping HTML tags, to avoid creating accidental elements
+        .replace(/&gt;/g, ">")
+        .replace(/&quot;/g, "\"")
+        .replace(/&apos;/g, "'")
+        .replace(/&#39;/g, "'")
+        .replace(/&amp;/g, "&") // must be last, to avoid creating accidental character entities
+
+    return normalizeWhitespace(html)
+}
+
+function iterateComments(document: Document, callback: (node: Comment) => void) {
+    const nodeIterator = document.createNodeIterator(document, NodeFilter.SHOW_COMMENT)
+    let currentNode: Node | null = null
+    while ((currentNode = nodeIterator.nextNode())) {
+        callback(currentNode as Comment)
+    }
+}
+
+export function normalizeWhitespace(s: string): string {
+    return s.replace(/\s+/g, " ").trim()
+}
